a39f25c7a029c88cd25f81d118fc576690506b7f
izzah salam <izzahsalam00@gmail.com>
2023-09-14 22:46:53 +0500

    Simple Weather App using API

--- app.js
+++ app.js
@@ -12,6 +12,50 @@ formEl.addEventListener("submit", (event) => {
 	getWheatherData(CityValue);
 });
 
+// async function getWheatherData(CityValue) {
+// 	try {
+// 		const response = await fetch(
+// 			`https://api.openweathermap.org/data/2.5/weather?q=${CityValue}&appid=${apiKey}&units=metric`
+// 		);
+// 		if (!response.ok) {
+// 			throw new Error("Network response was not ok");
+// 		}
+// 		const data = await response.json();
+// 		console.log(data);
+
+// 		const temperature = Math.round(data.main.temp);
+
+// 		const description = data.weather[0].description;
+
+// 		const icon = data.weather[0].icon;
+// 		const details = [
+// 			`Feels like : ${Math.round(data.main.feels_like)}°C`,
+// 			`
+//                           Humadity : ${data.main.humidity}%`,
+// 			`Wind speed : ${data.wind.speed}m/s`,
+// 		];
+
+// 		WheatherData.querySelector(".icon").innerHTML = `<img
+// 						src=" https://openweathermap.org/img/wn/${icon}.png"
+// 						alt="Wheter icon"
+// 					/>`;
+// 		WheatherData.querySelector(
+// 			".temperature"
+// 		).innerHTML = `${temperature} <span class="degree-sybmol">°C</span>`;
+
+// 		WheatherData.querySelector(".description").textContent = description;
+
+// 		WheatherData.querySelector(".details").innerHTML = details
+// 			.map(
+// 				(details) =>
+// 					`<div>
+// 					 ${details}
+// 				</div>`
+// 			)
+// 			.join("");
+// 	} catch (error) {}
+// }
+
 async function getWheatherData(CityValue) {
 	try {
 		const response = await fetch(
@@ -22,8 +66,11 @@ async function getWheatherData(CityValue) {
 		}
 		const data = await response.json();
 		console.log(data);
+
 		const temperature = Math.round(data.main.temp);
+
 		const description = data.weather[0].description;
+
 		const icon = data.weather[0].icon;
 		const details = [
 			`Feels like : ${Math.round(data.main.feels_like)}°C`,
@@ -41,6 +88,7 @@ async function getWheatherData(CityValue) {
 		).innerHTML = `${temperature} <span class="degree-sybmol">°C</span>`;
 
 		WheatherData.querySelector(".description").textContent = description;
+
 		WheatherData.querySelector(".details").innerHTML = details
 			.map(
 				(details) =>
